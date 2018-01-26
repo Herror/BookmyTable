de9177c76cb89d1a1e538b3e91d7bdb3a4bf72d0
Alin <enachi.alin@gmail.com>
2018-01-26 23:56:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/bookmytable/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.bookmytable;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting up a FAB to open the Editor / Detail Activity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
